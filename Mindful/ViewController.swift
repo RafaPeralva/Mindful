5975c2473ee49475e647762da0850c86be8710f1
Rafaela Peralva <rafaelaperalvag@gmail.com>
2021-11-05 13:26:27 -0400

    Initial Commit

---
+++ Mindful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mindful
+//
+//  Created by Rafaela Peralva on 11/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
